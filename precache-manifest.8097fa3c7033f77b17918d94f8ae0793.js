a60428a0105cccad1a4bec1df1dbf8988e1e8ba3
Rayyan Karim <rkarim0598@gmail.com>
2020-07-16 04:51:53 -0400

    Updates

---
+++ precache-manifest.8097fa3c7033f77b17918d94f8ae0793.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c5209357f3061c67ffb488615f6f483",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eadd83a19881c8771e55",
+    "url": "/static/css/main.6c2a1bf3.chunk.css"
+  },
+  {
+    "revision": "7994b867a3e93730e5a1",
+    "url": "/static/js/2.e1b4f48d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e1b4f48d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eadd83a19881c8771e55",
+    "url": "/static/js/main.5e4899f5.chunk.js"
+  },
+  {
+    "revision": "7fd50307644dd3303e46",
+    "url": "/static/js/runtime-main.bfcebc03.js"
+  },
+  {
+    "revision": "7bf99c2c5a7bb855dd0998bc10c00fd7",
+    "url": "/static/media/Calamity-Bold.7bf99c2c.ttf"
+  },
+  {
+    "revision": "0bb7ec1962e177065d6604423ae8ffd2",
+    "url": "/static/media/Calamity-Regular.0bb7ec19.ttf"
+  },
+  {
+    "revision": "6ebd1c0ff1b2d66ee7632754078faeb5",
+    "url": "/static/media/HyliaSerifBeta-Regular.6ebd1c0f.ttf"
+  },
+  {
+    "revision": "e5834a5ec166bb9da9b86b1c27fd316d",
+    "url": "/static/media/main-mobile.e5834a5e.jpg"
+  },
+  {
+    "revision": "48c3c4538a4f24b43866c48e09394f79",
+    "url": "/static/media/main.48c3c453.jpg"
+  }
+]);
