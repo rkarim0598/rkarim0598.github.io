5e1572459b57e006d2cc83eb0c17fdd9206fbdeb
Rayyan Karim <rkarim0598@gmail.com>
2020-07-17 04:20:20 -0400

    Add favicons and experience descriptions

--- app.js
+++
@@ -1,8 +0,0 @@
-import Router from './classes/Router.js';
-import IconManager from './classes/IconManager.js';
-export default class Store {
-    constructor() {
-        this.router = new Router();
-        this.iconManager = new IconManager();
-    }
-}
--- src/Views/Experience.js
+++ src/Views/Experience.js
@@ -17,14 +17,14 @@ export default function Experience(props) {
                 transform: 'translate(-50%, -50%)',
                 top: '50%',
                 height: '80%',
-                width: '80%',
+                width: '90%',
                 minWidth: '300px',
                 color: 'white',
                 alignItems: 'center',
                 justifyContent: 'space-around',
             }}
         >
-            <div style={{ height: '5%' }}>
+            <div style={{ height: '5%', fontSize: 'clamp(16px, 3vmin, 3vmin)' }}>
                 My Experience
             </div>
             <div className="experiences-container" style={{ overflowY: 'scroll', scrollbarColor: 'rgba(0, 0, 0, 0.3) transparent', fontSize: '3vmin', display: 'flex', flex: 1, width: '100%', height: '50%', maxHeight: '60vh', flexDirection: 'column', alignItems: 'center', paddingTop: '3px' }}>
@@ -43,12 +43,20 @@ export default function Experience(props) {
                         </div>
                         <div style={{ padding: '5px, 0, 0, 5px', marginLeft: '5px', display: 'flex', flexDirection: 'column', flex: 1, height: '100%', justifyContent: 'space-between' }}>
                             <div style={{ display: 'flex', flexDirection: 'column', height: '75%' }}>
-                                <span style={{ lineHeight: '170%', fontSize: 'clamp(10px, 2.5vmin, 16px)' }}>{exp.date}</span>
-                                <span style={{ lineHeight: '100%', fontFamily: 'Calamity-Bold', fontSize: 'clamp(10px, 2.5vmin, 16px)' }}>{exp.title} @ {exp.company}</span>
+                                <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', lineHeight: '115%', fontSize: 'clamp(13px, 2.5vmin, 16px)' }}>
+                                    <span style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis'}}>{exp.company}</span>
+                                    <span>{exp.date}</span>
+                                </div>
+                                <span style={{ lineHeight: '100%', fontFamily: 'Calamity-Bold', fontSize: 'clamp(13px, 2.5vmin, 16px)' }}>{exp.title}</span>
+                                <ul style={{ margin: 0, paddingLeft: '2.5vmin', color: '#cccccc', fontSize: 'clamp(12px, 2.5vmin, 14px)' }}>
+                                    {exp.description.map(desc => 
+                                        <li style={{ padding: 0, lineHeight: '100%', paddingTop: '5px', margin: 0 }}>{desc}</li>
+                                    )}
+                                </ul>
                             </div>
-                            <div style={{ display: 'flex', alignItems: 'right', width: '35%', alignSelf: 'flex-end', height: '25%' }}>
+                            <div style={{ display: 'flex', alignItems: 'right', width: '35%', alignSelf: 'flex-end', height: '20%' }}>
                                 {exp.current && <div className="shape">
-                                    <div className="top" style={{ textAlign: 'center', lineHeight: '100%' }}>Current</div>
+                                    <div className="top" style={{ textAlign: 'center', lineHeight: '160%', fontSize: 'clamp(12px, 2.5vmin, 14px)' }}>Current</div>
                                     <div className="bottom"></div>
                                 </div>}
                             </div>
@@ -59,7 +67,7 @@ export default function Experience(props) {
             </div>
             <button
                 onClick={() => props.setCurrentScreen('home')}
-                style={{ padding: 0, backgroundColor: 'transparent', margin: 0, border: 'none', color: 'white' }}>Back</button>
+                style={{ fontStyle: 'italic', fontSize: 'clamp(14px, 2.5vmin, 2.5vmin)', padding: 0, backgroundColor: 'transparent', margin: 0, border: 'none', color: 'white' }}>Back</button>
         </div>
     )
 }
