54ce5edd9052c8d9cc18eadc1a5c67fe3627214b
Rayyan Karim <rkarim0598@gmail.com>
2020-07-17 00:00:09 -0400

    Add company images to experience

--- src/Views/Experience.js
+++ src/Views/Experience.js
@@ -36,10 +36,12 @@ export default function Experience(props) {
                     >
                         <div id="top-left" className="animated-triangle upLeft"></div>
                         <div id="top-right" className="animated-triangle upRight"></div>
-                        <div id="bottom-left"  className="animated-triangle downLeft"></div>
+                        <div id="bottom-left" className="animated-triangle downLeft"></div>
                         <div id="bottom-right" className="animated-triangle downRight"></div>
-                        <img style={{ display: 'flex', alignSelf: 'flex-start', height: '100%', marginRight: '5px' }} src={require(`../assets/${exp.image}`)} alt='eventurely' />
-                        <div style={{ padding: '5px, 0, 0, 5px', display: 'flex', flexDirection: 'column', flex: 1, height: '100%', justifyContent: 'space-between' }}>
+                        <div style={{ display: 'flex', alignSelf: 'flex-start', height: '100%', backgroundColor: exp.backgroundColor }}>
+                            <img style={{ display: 'flex', alignSelf: 'flex-start', height: '100%', color: exp.backgroundColor }} src={require(`../assets/${exp.image}`)} alt='eventurely' />
+                        </div>
+                        <div style={{ padding: '5px, 0, 0, 5px', marginLeft: '5px', display: 'flex', flexDirection: 'column', flex: 1, height: '100%', justifyContent: 'space-between' }}>
                             <div style={{ display: 'flex', flexDirection: 'column', height: '75%' }}>
                                 <span style={{ lineHeight: '170%', fontSize: 'clamp(10px, 2.5vmin, 16px)' }}>{exp.date}</span>
                                 <span style={{ lineHeight: '100%', fontFamily: 'Calamity-Bold', fontSize: 'clamp(10px, 2.5vmin, 16px)' }}>{exp.title} @ {exp.company}</span>
