1ba1252897f0ae6a73bd17f1e2e559652d8de42e
Rayyan Karim <rkarim0598@gmail.com>
2020-07-19 01:33:21 -0400

    Handle browser back event

--- src/hooks/useRouter.js
+++ src/hooks/useRouter.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 export default function useRouter() {
     const [currentScreen, setCurrentScreen] = useState('');
 
-    useEffect(() => {
+    const determineCurrentScreen = () => {
         const path = window.location.href.split('/')[3];
         setCurrentScreen(path === 'experience' ?
             'experience' :
@@ -13,6 +13,14 @@ export default function useRouter() {
                     'play' :
                     'home'
         );
+    }
+
+    useEffect(() => {
+        determineCurrentScreen();
+
+        return window.addEventListener('popstate', (e) => { 
+            determineCurrentScreen(); 
+        });
     }, [])
 
     useEffect(() => {
